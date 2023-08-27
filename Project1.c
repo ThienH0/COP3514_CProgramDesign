33001e532ca890de85263044cd4b5aaa9b9bcb2c
Thien Ho <hqt1@usf.edu>
2023-08-27 15:17:14 -0400

    Add files via upload

---
+++ Project1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    int equipment_num;
+    printf("Please select from four equipment: 1, 2, 3, and 4\n");
+    printf("Enter equipment selection: %d\n", equipment_num);
+    return 0;
+}
