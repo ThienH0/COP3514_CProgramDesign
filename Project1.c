412a1ce829c4431009a3b943fd3adecacba124c4
Thien Ho <hqt1@usf.edu>
2023-08-28 13:35:27 -0400

    Added if statement and scanf.

--- Project1.c
+++ Project1.c
@@ -2,8 +2,22 @@
 
 int main()
 {
-    int equipment_num;
+    int equipment_num, num_days, num_hrs;;
+
+
     printf("Please select from four equipment: 1, 2, 3, and 4\n");
     printf("Enter equipment selection: %d\n", equipment_num);
+    scanf("%d", &equipment_num);
+
+    if (equipment_num < 1 || equipment_num > 4)
+    {
+        printf("Invalid selection. Select from 1 to 4.\n");
+        return 0;
+    }
+
+    printf("Enter days: %d\n", num_days);
+    scanf("%d", &num_days);
+
     return 0;
+
 }
