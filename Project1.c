40cf80aac7783010a74ff86db3ff5034f4bfbc42
Thien Ho <hqt1@usf.edu>
2023-08-30 16:46:20 -0400

    added more variables and conditions for Equipment 1

--- Project1.c
+++ Project1.c
@@ -2,11 +2,10 @@
 
 int main()
 {
-    int equipment_num, num_days, num_hrs;
+    int equipment_num, num_days, num_week, num_hrs;
+    int hr_rate = 0, day_rate = 0, wk_rate = 0, total;
     int equip1_4hr_rate = 7, equip1_daily_rate = 10, equip1_weekly_rate = 40;
-    int equip2_4hr_rate = 27, equip2_daily_rate = 39, equip2_weekly_rate = 156;
-    int equip3_4hr_rate = 61, equip3_daily_rate = 87, equip3_weekly_rate = 348;
-    int equip4_4hr_rate = 59, equip4_daily_rate = 84, equip4_weekly_rate = 336;
+    
 
 
     printf("Please select from four equipment: 1, 2, 3, and 4\n");
@@ -22,15 +21,36 @@ int main()
     printf("Enter days: %d\n", num_days);
     scanf("%d", &num_days);
 
-    if (num_hrs < 0 || num_hrs > 24)
-    {
+    if (num_days > 7){
+        num_week = num_days / 7;
+        num_days = num_days % 7;
+    }
+
+    printf("Enter hours: %d\n", num_hrs);
+    scanf("%d", &num_hrs);
+
+
+    if (num_hrs < 0 || num_hrs > 24){
         printf("Invalid hours.\n");
         return 0;
     }
 
     switch (equipment_num) {
+
         case 1:
-            if
+            if (num_hrs < 4) {
+                hr_rate = equip1_4hr_rate;
+            }
+            if (num_hrs < 24 || num_days < 1) {
+                day_rate = equip1_daily_rate;
+            }
+            if (num_week < 1) {
+                wk_rate = equip1_weekly_rate;
+            }
+            total = hr_rate + day_rate + wk_rate;
+            break;
     }
 
+    printf("Charge($): %d", total);
+    return 0;
 }
