b9b626d174d2cb23ee1babfa7471eac3a55378b9
Thien Ho <hqt1@usf.edu>
2023-08-30 16:58:03 -0400

    fixed some errors

--- Project1.c
+++ Project1.c
@@ -2,11 +2,12 @@
 
 int main()
 {
-    int equipment_num, num_days, num_week, num_hrs;
-    int hr_rate = 0, day_rate = 0, wk_rate = 0, total;
+    int equipment_num = 0, num_days = 0, num_week = 0, num_hrs = 0;
+    int hr_rate = 0, day_rate = 0, wk_rate = 0, total = 0;
     int equip1_4hr_rate = 7, equip1_daily_rate = 10, equip1_weekly_rate = 40;
-    
-
+    int equip2_4hr_rate = 27, equip2_daily_rate = 39, equip2_weekly_rate = 156;
+    int equip3_4hr_rate = 61, equip3_daily_rate = 87, equip3_weekly_rate = 348;
+    int equip4_4hr_rate = 59, equip4_daily_rate = 84, equip4_weekly_rate = 336;
 
     printf("Please select from four equipment: 1, 2, 3, and 4\n");
     printf("Enter equipment selection: %d\n", equipment_num);
@@ -18,15 +19,18 @@ int main()
         return 0;
     }
 
-    printf("Enter days: %d\n", num_days);
+    printf("Enter days: ");
     scanf("%d", &num_days);
 
     if (num_days > 7){
         num_week = num_days / 7;
         num_days = num_days % 7;
     }
+    else {
+        num_week = 0;
+    }
 
-    printf("Enter hours: %d\n", num_hrs);
+    printf("Enter hours: ");
     scanf("%d", &num_hrs);
 
 
@@ -49,8 +53,49 @@ int main()
             }
             total = hr_rate + day_rate + wk_rate;
             break;
+    
+
+        case 2:
+            if (num_hrs < 4) {
+                hr_rate = equip2_4hr_rate;
+            }
+            if (num_hrs < 24 || num_days < 1) {
+                day_rate = equip2_daily_rate;
+            }
+            if (num_week < 1) {
+                wk_rate = equip2_weekly_rate;
+            }
+            total = hr_rate + day_rate + wk_rate;
+            break;
+    
+
+        case 3:
+            if (num_hrs < 4) {
+                hr_rate = equip3_4hr_rate;
+            }
+            if (num_hrs < 24 || num_days < 1) {
+                day_rate = equip3_daily_rate;
+            }
+            if (num_week < 1) {
+                wk_rate = equip3_weekly_rate;
+            }
+            total = hr_rate + day_rate + wk_rate;
+            break;
+    
+        case 4:
+            if (num_hrs < 4) {
+                hr_rate = equip4_4hr_rate;
+            }
+            if (num_hrs < 24 || num_days < 1) {
+                day_rate = equip4_daily_rate;
+            }
+            if (num_week < 1) {
+                wk_rate = equip4_weekly_rate;
+            }
+            total = hr_rate + day_rate + wk_rate;
+            break;
     }
 
-    printf("Charge($): %d", total);
+    printf("Charge($): %d\n", total);
     return 0;
 }
